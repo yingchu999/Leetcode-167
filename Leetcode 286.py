53b27e4e24f98ead094316332cbe41002b194b47
yingchu999 <74446526+yingchu999@users.noreply.github.com>
2020-11-21 21:00:33 -0800

    Update and rename Leetcode 286 to Leetcode 286.py

--- Leetcode 286.py
+++ Leetcode 286.py
@@ -10,19 +10,23 @@ for i in range(m):
     for j in range(n):
         if rooms[i][j] == 0:
             queue = collections.deque()
-            queue.append((i+1,j,1))
-            queue.append((i,j+1,1))
-            queue.append((i-1,j,1))
-            queue.append((i,j-1,1))
+            queue = self.A(i,j,1,m,n,queue)
             see = set()
             while queue:
                 a,b,val = queue.popleft()
-                if a<0 or a>=m or b<0 or b>=n or rooms[a][b] in [0,-1] or (a,b) in see:
+                if rooms[a][b] in [0,-1] or (a,b) in see:
                     continue
-
                 rooms[a][b] = min(rooms[a][b],val)
                 see.add((a,b))
-                queue.append((a+1,b,val+1))
-                queue.append((a,b+1,val+1))
-                queue.append((a-1,b,val+1))
-                queue.append((a,b-1,val+1))
+                queue = self.A(a,b,val+1,m,n,queue)
+                        
+def A(self,i,j,val,m,n,queue):
+    if i+1<m:
+        queue.append((i+1,j,val))
+    if j+1<n:
+        queue.append((i,j+1,val))
+    if i-1>=0:
+        queue.append((i-1,j,val))
+    if j-1>=0:
+        queue.append((i,j-1,val))
+    return queue
